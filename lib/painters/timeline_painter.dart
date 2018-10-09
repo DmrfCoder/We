0ad02e81d045b73af133ab7448f5739a3af50181
xuefanggang <xuefanggang97@gmail.com>
2018-10-09 23:39:26 +0800

    android icon bug

--- lib/painters/timeline_painter.dart
+++ lib/painters/timeline_painter.dart
@@ -13,10 +13,11 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class TimelinePainter extends CustomPainter {
 
+class TimelinePainter extends CustomPainter {
   final Color lineColor;
   final Color backgroundColor;
   final bool firstElement;
@@ -24,79 +25,68 @@ class TimelinePainter extends CustomPainter {
   final Animation<double> controller;
   final Animation<double> height;
 
-  TimelinePainter({
-    @required this.lineColor,
-    @required this.backgroundColor,
-    this.firstElement = false,
-    this.lastElement = false,
-    this.controller
-  }) :height = new Tween(begin: 0.0, end: 1.0).animate(new CurvedAnimation(
-          parent: controller,
-          curve: new Interval(
-            0.45, 1.0,
-            curve: Curves.ease),
+  TimelinePainter(
+      {@required this.lineColor,
+      @required this.backgroundColor,
+      this.firstElement = false,
+      this.lastElement = false,
+      this.controller})
+      : height = new Tween(begin: 0.0, end: 1.0).animate(
+          new CurvedAnimation(
+            parent: controller,
+            curve: new Interval(0.45, 1.0, curve: Curves.ease),
+          ),
         ),
-      ),
-      super(repaint: controller);
+        super(repaint: controller);
 
   @override
   void paint(Canvas canvas, Size size) {
     _centerElementPaint(canvas, size);
   }
 
-  void _centerElementPaint(Canvas canvas, Size size)
-  {
+  void _centerElementPaint(Canvas canvas, Size size) {
     Paint lineStroke = new Paint()
-    ..color = lineColor
-    ..strokeCap = StrokeCap.round
-    ..strokeWidth = 2.0
-    ..style = PaintingStyle.stroke;
-    if(firstElement && lastElement)
-    {
+      ..color = lineColor
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 2.0
+      ..style = PaintingStyle.stroke;
+    if (firstElement && lastElement) {
       // Do nothing
-    }
-    else if(firstElement)
-    {
+    } else if (firstElement) {
       Offset offsetCenter = size.center(new Offset(0.0, -4.0));
       Offset offsetBottom = size.bottomCenter(new Offset(0.0, 0.0));
-      Offset renderOffset = new Offset(offsetBottom.dx, offsetBottom.dy*(0.5+(controller.value/2)));
-      canvas.drawLine(
-      offsetCenter, 
-      renderOffset, 
-      lineStroke);
-    }
-    else if(lastElement)
-    {
+      Offset renderOffset = new Offset(
+          offsetBottom.dx, offsetBottom.dy * (0.5 + (controller.value / 2)));
+
+      canvas.drawLine(offsetCenter, renderOffset, lineStroke);
+    } else if (lastElement) {
       Offset offsetTopCenter = size.topCenter(new Offset(0.0, 0.0));
       Offset offsetCenter = size.center(new Offset(0.0, -4.0));
-      Offset renderOffset = new Offset(offsetCenter.dx, offsetCenter.dy*controller.value);
-      canvas.drawLine(
-      offsetTopCenter, 
-      renderOffset, 
-      lineStroke);
-    }
-    else {
+      Offset renderOffset =
+          new Offset(offsetCenter.dx, offsetCenter.dy * controller.value);
+      canvas.drawLine(offsetTopCenter, renderOffset, lineStroke);
+    } else {
       Offset offsetTopCenter = size.topCenter(new Offset(0.0, 0.0));
       Offset offsetBottom = size.bottomCenter(new Offset(0.0, 0.0));
-      Offset renderOffset = new Offset(offsetBottom.dx, offsetBottom.dy*controller.value);
-      canvas.drawLine(
-      offsetTopCenter, 
-      renderOffset, 
-      lineStroke);
+      Offset renderOffset =
+          new Offset(offsetBottom.dx, offsetBottom.dy * controller.value);
+      canvas.drawLine(offsetTopCenter, renderOffset, lineStroke);
     }
-    
-    
+
     Paint circleFill = new Paint()
-    ..color = lineColor
-    ..style = PaintingStyle.fill;
+      ..color = lineColor
+      ..style = PaintingStyle.fill;
+
+
+
+
 
     canvas.drawCircle(size.center(new Offset(0.0, -8.0)), 6.0, circleFill);
-
   }
 
   @override
   bool shouldRepaint(TimelinePainter oldDelegate) {
-    return oldDelegate.lineColor!=lineColor || oldDelegate.backgroundColor!=backgroundColor;
+    return oldDelegate.lineColor != lineColor ||
+        oldDelegate.backgroundColor != backgroundColor;
   }
-
 }
